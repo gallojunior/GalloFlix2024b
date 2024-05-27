aba9e4cbd45aed08786abb2e921fb8e6e9b4ef6d
GalloJunior <gallojunior@gmail.com>
2024-05-27 07:01:14 -0300

    Finalizando login

--- GalloFlix/Controllers/AccountController.cs
+++ GalloFlix/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 using System.Net.Mail;
+using System.Security.Claims;
 using GalloFlix.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -37,16 +38,48 @@ public class AccountController : Controller
     {
         if (ModelState.IsValid)
         {
+            string userName = login.Email;
+            if (IsValidEmail(userName))
+            {
+                var user = await _userManager.FindByEmailAsync(userName);
+                if (user != null)
+                    userName = user.UserName;
+            }
 
+            var result = await _signInManager.PasswordSignInAsync(
+                userName, login.Password, login.RememberMe, lockoutOnFailure: true
+            );
+
+            if (result.Succeeded)
+            {
+                _logger.LogInformation($"Usuário {userName} fez login");
+                return LocalRedirect(login.ReturnUrl);
+            }
+
+            if (result.IsLockedOut) 
+            {
+                _logger.LogWarning($"Usuário {userName} foi bloqueado");
+                ModelState.AddModelError(string.Empty, "Conta Bloqueada! Aguarde alguns minutos para continuar!");
+            }
+            else
+                ModelState.AddModelError(string.Empty, "Usuário e/ou Senha Inválidos!");
         }
         return View(login);
     }
 
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Logout()
     {
-        return View("Error!");
+        _logger.LogInformation($"Usuario {ClaimTypes.Email} fez logoff");
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+
+    public IActionResult AccessDenied()
+    {
+        return View();
     }
 
     private static bool IsValidEmail(string email)
